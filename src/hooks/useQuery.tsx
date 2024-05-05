c3113078b1e556393d535d61f5eb9548cc8f96d5
ShivamTaneja <shiivamtaneja@gmail.com>
2024-05-05 13:34:40 +0530

    add custom hook
    Signed-off-by: ShivamTaneja <shiivamtaneja@gmail.com>

---
+++ src/hooks/useQuery.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+const useQuery = <T,>({
+  queryFn,
+  options,
+  errMsg,
+  status
+}: {
+  queryFn: string,
+  options: RequestInit,
+  errMsg: string,
+  status: "authenticated" | "loading" | "unauthenticated"
+}) => {
+  const controllerRef = useRef<AbortController>();
+
+  const [data, setData] = useState<{
+    message: string,
+    data: T | null
+  }>({
+    message: '',
+    data: null
+  });
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (status === 'loading' || status === 'unauthenticated')
+      return;
+
+    controllerRef.current = new AbortController();
+    const signal = controllerRef.current.signal;
+
+    const getData = async () => {
+      try {
+        const response = await fetch('/api' + queryFn, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include',
+          ...options,
+          signal
+        });
+
+        if (!response.ok) {
+          const error = await response.json();
+          throw new Error(error.error);
+        }
+
+        const data = await response.json();
+
+        setData(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.message != 'The user aborted a request.') {
+            setIsError(`${errMsg}: ${error.message}`);
+          }
+        }
+        else {
+          setIsError(`${errMsg}: Unknown error`);
+        }
+      }
+    };
+
+    getData();
+
+    return () => {
+      if (controllerRef.current) {
+        controllerRef.current.abort();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    isError,
+    data
+  };
+};
+
+export default useQuery;
